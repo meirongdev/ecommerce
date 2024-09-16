91d8d9d9b2a67ac4f3ba7ef9fb2be12cb7d30619
Meirong <meirongdev@gmail.com>
2024-09-16 10:44:07 +0800

    feat: use spring data rest

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	// https://github.com/spring-projects/spring-boot/issues/39753
 	// can not use 3.2.0 now
 	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0"){
--- src/main/java/dev/meirong/ecommerce/respository/CarRepository.java
+++ src/main/java/dev/meirong/ecommerce/respository/CarRepository.java
@@ -3,10 +3,12 @@ package dev.meirong.ecommerce.respository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import dev.meirong.ecommerce.domain.Car;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 	// Fetch cars by brand
-	List<Car> findByBrand(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
 }
